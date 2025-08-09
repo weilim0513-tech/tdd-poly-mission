28a6a2ea7a301ff34768e17534f9db933143e1fe
rwl <weilim0513@gmail.com>
2025-08-09 13:06:46 +0900

    t2: Refactor

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -1,8 +1,13 @@
 public class Calc {
     static int run(String polynomial) {
-        if(polynomial.equals("1 + 1")) return 2;
-        if(polynomial.equals("2 + 1")) return 3;
+        // 앞의 숫자와 뒤의 숫자의 합에 따라 결과 값이 달라져야 함
+        // String에서 숫자 부분을 캐치하여 int형으로 컨버팅 해야겠음
+        int sum = 0;
 
-        return 2;
+        for(char c : polynomial.toCharArray()) {
+            if(Character.isDigit(c)) sum += Character.getNumericValue(c);
+        }
+
+        return sum;
     }
 }
