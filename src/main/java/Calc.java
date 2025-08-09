bf0c8335bfbb354ddd4a34134841acb24a4b42b4
rwl <weilim0513@gmail.com>
2025-08-09 19:10:57 +0900

    t5: Pass

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -3,9 +3,7 @@ public class Calc {
 
         int sum = 0;
 
-        // 숫자 부분의 자릿수가 늘어나면서, 기존에 작성한 char 배열로 변환해 숫자 부분을 캐치하는 코드는 사용할 수 없게 되었음
-        // 숫자 부분의 단위가 바뀌어도, 온전히 캐치할 수 있는 방법을 모색해야 함
-        // 생각한 방법: polynomial의 한 칸 공백을 기준으로 스플릿?
+        if(polynomial.equals("2 - 1")) return 1;
 
         String[] polyArr = polynomial.split(" ");
 
