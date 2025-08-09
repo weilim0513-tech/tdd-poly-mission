0fa9de4cb0fe1b079af4a1fc2e47765a5f72e346
rwl <weilim0513@gmail.com>
2025-08-09 13:09:08 +0900

    t4: Fail

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -16,4 +16,16 @@ public class CalcTest {
     void t2() {
         assertThat(Calc.run("2 + 1")).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("2 + 2 == 4")
+    void t3() {
+        assertThat(Calc.run("2 + 2")).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("1000 + 280 == 1280")
+    void t4() {
+        assertThat(Calc.run("1000 + 280")).isEqualTo(1280);
+    }
 }
