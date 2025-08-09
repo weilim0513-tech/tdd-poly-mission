88a8288f42e111ed08edc031c7b68f4147bb84f8
rwl <weilim0513@gmail.com>
2025-08-09 19:06:23 +0900

    t4: Refactor

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -3,10 +3,18 @@ public class Calc {
 
         int sum = 0;
 
-        if(polynomial.equals("1000 + 280")) return 1280;
+        // 숫자 부분의 자릿수가 늘어나면서, 기존에 작성한 char 배열로 변환해 숫자 부분을 캐치하는 코드는 사용할 수 없게 되었음
+        // 숫자 부분의 단위가 바뀌어도, 온전히 캐치할 수 있는 방법을 모색해야 함
+        // 생각한 방법: polynomial의 한 칸 공백을 기준으로 스플릿?
 
-        for(char c : polynomial.toCharArray()) {
-            if(Character.isDigit(c)) sum += Character.getNumericValue(c);
+        String[] polyArr = polynomial.split(" ");
+
+        for(String s : polyArr) {
+            try {
+                sum += Integer.parseInt(s);
+            } catch(NumberFormatException e){
+                continue;
+            }
         }
 
         return sum;
